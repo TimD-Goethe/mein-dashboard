e1822b80d4c478c3825e4bb7681dbf15f0945f24
TimD-Goethe <Detrois@wiwi.uni-frankfurt.de>
2025-05-15 18:58:03 +0200

    Update url5.py

--- url5.py
+++ url5.py
@@ -20,7 +20,7 @@ st.markdown(
     """
     <style>
       .block-container {
-        padding-top: 2.5 rem;
+        padding-top: 0 rem;
       }
     /* 1) Full-page Gradient auf den App-Hintergrund */
       html, body, [data-testid="stAppViewContainer"], .block-container {
