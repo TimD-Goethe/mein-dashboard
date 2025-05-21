a7805b2ef84e44e8b9b1ed21cce2d96e03c94498
TimD-Goethe <Detrois@wiwi.uni-frankfurt.de>
2025-05-21 13:42:09 +0200

    Update url5.py

--- url5.py
+++ url5.py
@@ -1751,7 +1751,7 @@ with main:
                 # 7) Linien & Styling
                 overall_avg = df["num_o_seit_500"].mean()
                 fig_s.add_vline(
-                    x=overall_avg,
+                    x=super_avg,
                     line_dash="dash",
                     line_color="black",
                     line_width=2,
